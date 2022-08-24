e40c10d7c8b4679544374c4a27aeafbec9fd1ab2
thiagola92 <thiagola92@gmail.com>
2022-08-24 19:13:31 -0300

    Change to use context manager from Catch instead of decorator

--- url_parser/parser.py
+++ url_parser/parser.py
@@ -1,6 +1,6 @@
 import re
 
-from la_catch import catch
+from la_catch import Catch
 from structlog.stdlib import BoundLogger, get_logger
 
 from url_parser.models.query_url import QueryUrl
@@ -16,29 +16,15 @@ class Parser:
     def __init__(self, logger: BoundLogger = get_logger()) -> None:
         self._logger = logger
 
-    def _log_parse_error(
-        self, url: str, marketplace: str, exception: Exception
-    ) -> None:
-        self._logger.exception(
-            event="Parser error",
-            url=url,
-            marketplace=marketplace,
-        )
-
-    @catch(call=_log_parse_error)
     def parse(self, url: str, marketplace: str) -> SkuUrl | QueryUrl | None:
         """Call the parse function from the respective marketplace."""
 
-        parser = get_marketplace_parser(marketplace=marketplace, logger=self._logger)
-        return parser.parse(url=url)
+        with Catch(callback=self._log_parse_error, url=url, marketplace=marketplace):
+            return get_marketplace_parser(
+                marketplace=marketplace,
+                logger=self._logger,
+            ).parse(url=url)
 
-    def _log_get_marketplace_error(self, url: str, exception: Exception) -> None:
-        self._logger.exception(
-            event="Get marketplace error",
-            url=url,
-        )
-
-    @catch(call=_log_get_marketplace_error)
     def get_marketplace(self, url: str) -> str | None:
         """
         Identifies the marketplace which the url belongs.
@@ -50,18 +36,34 @@ class Parser:
         https://www.google.com/search?q=https%3A//www.amazon.com.br
         """
 
-        if re.search(r"^http[s]?:\/\/www\.rihappy\.com\.br/", url):
-            return "rihappy"
-        elif re.search(r"^http[s]?:\/\/www\.amazon\.com\.br/", url):
-            return "amazon"
-        elif re.search(r"^http[s]?:\/\/www\.americanas\.com\.br/", url):
-            return "americanas"
-        elif re.search(r"^http[s]?:\/\/www\.shoptime\.com\.br/", url):
-            return "shoptime"
-        elif re.search(r"^http[s]?:\/\/www\.submarino\.com\.br/", url):
-            return "submarino"
-        elif re.search(r"^http[s]?:\/\/www\.magazineluiza\.com\.br/", url):
-            return "magalu"
-        elif re.search(r"^http[s]?:\/\/www\.magalu\.com\.br/", url):
-            return "magalu"
+        with Catch(callback=self._log_get_marketplace_error, url=url):
+            if re.search(r"^http[s]?:\/\/www\.rihappy\.com\.br/", url):
+                return "rihappy"
+            elif re.search(r"^http[s]?:\/\/www\.amazon\.com\.br/", url):
+                return "amazon"
+            elif re.search(r"^http[s]?:\/\/www\.americanas\.com\.br/", url):
+                return "americanas"
+            elif re.search(r"^http[s]?:\/\/www\.shoptime\.com\.br/", url):
+                return "shoptime"
+            elif re.search(r"^http[s]?:\/\/www\.submarino\.com\.br/", url):
+                return "submarino"
+            elif re.search(r"^http[s]?:\/\/www\.magazineluiza\.com\.br/", url):
+                return "magalu"
+            elif re.search(r"^http[s]?:\/\/www\.magalu\.com\.br/", url):
+                return "magalu"
         return None
+
+    def _log_parse_error(
+        self, url: str, marketplace: str, exception: Exception
+    ) -> None:
+        self._logger.exception(
+            event="Parser error",
+            url=url,
+            marketplace=marketplace,
+        )
+
+    def _log_get_marketplace_error(self, url: str, exception: Exception) -> None:
+        self._logger.exception(
+            event="Get marketplace error",
+            url=url,
+        )
